d6194083efc819f56f6fb600ba328ea7b357f4ad
azharrahad31 <azherudinrahad@gmail.com>
2023-07-01 21:14:21 +0600

    set input value

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,25 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputValue,setInputValue]=useState({
+    firstName: "",
+    lastName: "",
+  })
+  const handleChange=(e)=>{
+    setInputValue({
+      ...inputValue,
+      [e.target.name]: e.target.value
+    })
+    console.log(inputValue)
+
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <input type="text" name='firstName' placeholder='Type First Name' onChange={handleChange}/>
+     <input type="text" name="lastName" placeholder='Type last Name' onChange={handleChange}/>
+     <button>Post</button>
+
     </>
   )
 }
