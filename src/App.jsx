354885037417543ee255f031da599776efe30805
azharrahad31 <azherudinrahad@gmail.com>
2023-07-02 01:53:22 +0600

    user can update data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
-import {getDatabase, onValue, push, ref, remove, set} from 'firebase/database'
+import {getDatabase, onValue, push, ref, remove, set, update} from 'firebase/database'
 function App() {
   const db=getDatabase()
   const [inputValue,setInputValue]=useState({
     firstName: "",
     lastName: "",
   })
-  const [infoArr,setInfoArr]=useState([])
+  const [infoArr,setInfoArr]=useState([]);
+  const [id,setId]=useState("");
+
   const handleChange=(e)=>{
     setInputValue({
       ...inputValue,
@@ -37,15 +39,31 @@ function App() {
     remove(ref(db,'todoReact/' + id))
 
   }
+  const handleEdit=(item)=>{
+    setInputValue({
+      firstName: item.fName,
+      lastName: item.lName
+    })
+    setId(item.id)
+  }
+
+const handleUpdate=()=>{
+
+  update(ref(db,'todoReact/' + id),{
+    fName: inputValue.firstName,
+    lName:inputValue.lastName
+  })
+
+  
+}
   return (
     <>
-     <input type="text" name='firstName' placeholder='Type First Name' onChange={handleChange}/>
-     <input type="text" name="lastName" placeholder='Type last Name' onChange={handleChange}/>
+     <input type="text" name='firstName' placeholder='Type First Name' onChange={handleChange} value={inputValue.firstName}/>
+     <input type="text" name="lastName" placeholder='Type last Name' onChange={handleChange} value={inputValue.lastName}/>
      <button onClick={handlePost}>Post</button>
-      <ul>{infoArr.map((item,index)=><li key={index}>{item.fName}----{item.lName}---{item.id} <button>Edit</button><button onClick={()=>{handleDelete(item.id)}}>Delete</button></li>)
-        
-        
-        }</ul>
+     <button onClick={handleUpdate}>Update</button>
+      <ul>{infoArr.map((item,index)=><li key={index}>{item.fName}----{item.lName}---{item.id} <button onClick={()=>{handleEdit(item)}}>Edit</button><button onClick={()=>{handleDelete(item.id)}}>Delete</button></li>)
+         }</ul>
     </>
   )
 }
